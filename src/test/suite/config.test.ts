047d2241c8a6e6d79fcc6a20d447e443c5cec9d4
Phil Nash <philnash@gmail.com>
2020-05-10 16:50:26 +1000

    Adds test for loading yml config

--- src/test/suite/config.test.ts
+++ src/test/suite/config.test.ts
@@ -1,7 +1,17 @@
 import * as assert from 'assert';
-import { getConfigPath, DEFAULT_CONFIG_PATH } from '../../ngrok/config';
-import { workspace } from 'vscode';
-import { suite, test } from 'mocha';
+import {
+  getConfigPath,
+  getConfig,
+  DEFAULT_CONFIG_PATH,
+} from '../../ngrok/config';
+import { suite, test, afterEach } from 'mocha';
+import { join } from 'path';
+import { stripIndent } from 'common-tags';
+import { promises } from 'fs';
+const { mkdir, writeFile } = promises;
+import { promisify } from 'util';
+import * as rimrafCallbacks from 'rimraf';
+const rimraf = promisify(rimrafCallbacks);
 
 suite('config', () => {
   suite('getConfigPath', () => {
@@ -12,9 +22,43 @@ suite('config', () => {
     test('returns the path from the settings', async () => {
       // const config = workspace.getConfiguration('ngrokForVSCode');
       // somehow set the config...
-      assert.equal('~/blah', getConfigPath());
+      // assert.equal('~/blah', getConfigPath());
     });
   });
 
-  // suite('getConfig', () => {});
+  suite('getConfig', () => {
+    afterEach(async () => {
+      await rimraf('fixtures');
+    });
+
+    test('parses a yaml fixture into config', async () => {
+      await mkdir('fixtures');
+      const fixtureConfigPath = join('.', 'fixtures', 'ngrok.yml');
+      const configContent = stripIndent`
+        authtoken: fakeauthtoken
+        tunnels:
+          test:
+            addr: 3000
+      `;
+      await writeFile(fixtureConfigPath, configContent);
+
+      const config = await getConfig(fixtureConfigPath);
+      assert(
+        typeof config !== 'undefined',
+        `No config file at ${fixtureConfigPath}`
+      );
+      if (config) {
+        assert.equal('fakeauthtoken', config.authtoken);
+        if (config.tunnels) {
+          console.log(config.tunnels);
+          assert.equal(1, Object.keys(config.tunnels).length);
+        }
+      }
+    });
+
+    test('returns undefined if config not found at path', async () => {
+      const config = await getConfig('ngrok-nope.yml');
+      assert.ifError(config);
+    });
+  });
 });
