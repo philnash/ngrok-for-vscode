24bb4349cfa76c33601141dc55ba2026fcd82f3f
Phil Nash <philnash@gmail.com>
2020-12-31 10:14:01 +1100

    Updates ngrok node version

--- src/ngrok/statusBarItem.ts
+++ src/ngrok/statusBarItem.ts
@@ -5,7 +5,7 @@ export const createStatusBarItem = (commandId: string, version?: string) => {
   statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
   let statusBarText = '$(globe) ngrok';
   if (version) {
-    statusBarText += ` (v ${version})`;
+    statusBarText += ` (v${version})`;
   }
   statusBarItem.text = statusBarText;
   statusBarItem.tooltip = 'ngrok is running';
